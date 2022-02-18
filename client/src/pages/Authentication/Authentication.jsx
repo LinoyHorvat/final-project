ab8fc20036deb4cc883f0a683c21ee050493432a
LinoyHorvat <linoy.horvat@gmail.com>
2022-02-18 11:09:16 +0200

    authentication

--- client/src/pages/Authentication/Authentication.jsx
+++ client/src/pages/Authentication/Authentication.jsx
@@ -5,13 +5,30 @@ import React, { useState, useEffect } from "react";
 
 
 function Authentication() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const loginUser = async () => {
     try{
       const { data } = await myApi.post('/users/login', 
       {
-        email: "linoy@gmail.com",
-        password: "linoy123456"
+        email: email,
+        password: password
+      })
+      console.log(data);
+    }
+    catch(err){
+      console.log(err.response.data);
+    }
+  }
+  const createNewUser = async () => {
+    try{
+      const { data } = await myApi.post('/users/register', 
+      {
+        name,
+        email: email,
+        password: password
       })
       console.log(data);
     }
@@ -26,12 +43,13 @@ function Authentication() {
         <div className="heder">Sign In</div>
         <div className="inputsDiv">
           <div className="emailPassword">Email</div>
-          <input type="text" placeholder="Enter your email" />
+          <input type="text" placeholder="Enter your email" onChange = {e => setEmail(e.target.value)}/>
           <div className="emailPassword">Password</div>
           <input
             className="input"
             type="text"
             placeholder="Enter your password"
+            onChange = {(e)=>setPassword(e.target.value)}
           />
         </div>
         <button className="input" type="button" className="btn" onClick= {loginUser}>
@@ -42,24 +60,20 @@ function Authentication() {
         <div className="heder">Create Account</div>
         <div className="inputsDiv">
         <div className="emailPassword">Name</div>
-        <input type="text" placeholder="Enter your name" />
+        <input type="text" placeholder="Enter your name" onChange = {(e)=>setName(e.target.value)}/>
           <div className="emailPassword">Email</div>
-          <input type="text" placeholder="Enter your email" />
+          <input type="text" placeholder="Enter your email" onChange = {(e)=>setEmail(e.target.value)}/>
           <div className="emailPassword">Password</div>
           <input
             className="input"
             type="text"
             placeholder="Enter your password"
+            onChange = {(e)=>setPassword(e.target.value)}
           />
         </div>
-        <div className="buttonsDiv">
-        <button type="button" className="btn">
-          Find a room{" "}
-        </button>
-        <button type="button" className="btn">
-          List your room{" "}
-        </button>
-      </div>
+        <button className="input" type="button" className="btn" onClick= {createNewUser}>
+        Submit
+      </button>
       </div>
     </div>
   );
